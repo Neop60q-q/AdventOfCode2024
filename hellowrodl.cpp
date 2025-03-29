bf0cc2e67e0bef0c029f360ed5a9561dc0653ee7
Neop60q-q <thdtaka29@gmail.com>
2025-03-30 01:52:58 +1100

    final

--- hellowrodl.cpp
+++ hellowrodl.cpp
@@ -2,24 +2,42 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <fstream>
+#include <string>
 using namespace std;
 
 // plan:
 // create 2 vector for LeftList and RightList
 // use sort() to sort
 // retrieve "number pairing" from vector and calculate distance and therfore total distance
-
 // implement reading input file
 
 int main() {
-    std::cout << "Hello World!";
 
-    vector<int> LeftList = {3, 4, 2, 1, 3, 3};
-    vector<int> RightList = {4, 3, 5, 3, 9, 3};
+    vector<int> LeftList = {};
+    vector<int> RightList = {};
+
+/////////////////////////////////////////
+// File handling steps:
+// Create a stream object. 
+// Connect it to a file on disk. 
+// Read the file’s contents into our stream object. 
+// Close the file.
+    std::ifstream myfile; myfile.open("inputPart1.txt");
+
+    int left;
+    int right;
+    while (myfile >> left >> right){ 
+        LeftList.push_back(left); 
+        RightList.push_back(right);
+    }
+
+    myfile.close();
+
+/////////////////////////////////////////
 
     sort(LeftList.begin(), LeftList.end());
     sort(RightList.begin(), RightList.end());
-
 
 
     cout << "LeftList: ";
@@ -33,6 +51,18 @@ int main() {
         cout << num << " ";
     }
     cout << endl; 
+
+
+    int totaldiff = 0;
+    for (int i = 0; i < LeftList.size(); i++){ // For loop iterates over each element (startrange, stoprange, action)
+        int difference = max(LeftList[i], RightList[i]) - min (LeftList[i], RightList[i]);
+        totaldiff += difference;
+
+    };
+
+     std::cout << "Total Difference: " << totaldiff;
+
+/////////////////////////////////////////
 
     return 0;
 }
